bf08de6630db1ba4db99695e2f5a9c8e22dc4440
Martin Rodemann <martin@rodemann.org>
2014-04-14 15:35:55 +0200

    Add Battery Symbol PA style

--- src/net/purespeedx/psxxtras/StatusbarSettings.java
+++ src/net/purespeedx/psxxtras/StatusbarSettings.java
@@ -23,9 +23,11 @@ import net.purespeedx.psxxtras.R;
 public class StatusbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
 	private static final String KEY_SWIPE_FOR_QS = "swipe_for_qs";
-	
+	private static final String KEY_BATTERY_ICON = "battery_icon_title";
+    
 	private CheckBoxPreference mSwipeForQs;
-	
+	private ListPreference mBatteryIcon;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +41,41 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements OnP
 			mSwipeForQs.setChecked(Settings.System.getInt(getContentResolver(),
                                  Settings.System.QUICK_SETTINGS_QUICK_PULL_DOWN, 0) == 1);
 		}
-		
+	
+        mBatteryIcon = (ListPreference) findPreference(KEY_BATTERY_ICON);
+        if (mBatteryIcon !=null) {
+            mBatteryIcon.setOnPreferenceChangeListener(this);
+            int BatteryValue = Settings.System.getInt(getContentResolver(),
+                                 Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+            mBatteryIcon.setValue(String.valueOf(BatteryValue));
+            updateBatteryIcon(BatteryValue);            
+        }
     }
 
+   private String getBatterystring(int value) {
+        Resources res = getResources();
+        String menustate = "";
+        switch(value){
+        case 1:
+            menustate=res.getString(R.string.battery_bar_percent);
+            break;
+        case 2:
+            menustate=res.getString(R.string.battery_circle);
+            break;
+        case 3:
+            menustate=res.getString(R.string.battery_circle_percent);
+            break;
+        default:
+            menustate=res.getString(R.string.battery_bar);
+        }
+        return menustate;
+    }
+    
+    private void updateBatteryIcon(int value) {
+        mBatteryIcon.setSummary(getBatterystring(value));
+    }
+
+    
 	
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {      	
@@ -56,7 +90,13 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements OnP
             Settings.System.putInt(getContentResolver(),
                     Settings.System.QUICK_SETTINGS_QUICK_PULL_DOWN, val);
             return true;        
+        } else if (preference == mBatteryIcon) {
+            final int bValue = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_BATTERY_STYLE, bValue);
+            updateBatteryIcon(bValue);
+            return true;
         }
-        return false;
+            return false;
     }
 }
