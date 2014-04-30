679676a67c4e4de8be1a623622ab3f54a9922247
Martin Rodemann <martin@rodemann.org>
2014-04-30 18:08:54 +0200

    Settings:hold back to kill app (2/2)

--- src/net/purespeedx/psxxtras/NavbarSettings.java
+++ src/net/purespeedx/psxxtras/NavbarSettings.java
@@ -1,6 +1,7 @@
 package net.purespeedx.psxxtras;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Ringtone;
@@ -14,6 +15,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.widget.Toast;
 
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -22,41 +24,58 @@ import net.purespeedx.psxxtras.R;
 public class NavbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";	
+    private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 	
 	private ListPreference mNavigationBarHeight;
-	
+    private CheckBoxPreference mKillAppLongpressBack;	
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.navbar_settings);
 		PreferenceScreen prefSet = getPreferenceScreen();
-		
+		ContentResolver resolver = getActivity().getContentResolver();
+        
         // Navigationbar height
         mNavigationBarHeight = 
 		    (ListPreference) prefSet.findPreference(KEY_NAVIGATION_BAR_HEIGHT);
 	    if (mNavigationBarHeight != null) {
             mNavigationBarHeight.setOnPreferenceChangeListener(this);
-            int statusNavigationBarHeight = Settings.System.getInt(getActivity().getApplicationContext()
-                    .getContentResolver(),
+            int statusNavigationBarHeight = Settings.System.getInt(resolver,
                     Settings.System.NAVIGATION_BAR_HEIGHT, 48);
             mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
             mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());		
 		}
+        
+        mKillAppLongpressBack = (CheckBoxPreference) prefSet.findPreference(KILL_APP_LONGPRESS_BACK);
+        mKillAppLongpressBack.setChecked(Settings.System.getInt(
+                resolver, Settings.System.KILL_APP_LONGPRESS_BACK, 0) != 0);    
     }
 
+    
+   private void writeKillAppLongpressBackOptions() {
+        Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.KILL_APP_LONGPRESS_BACK,
+                mKillAppLongpressBack.isChecked() ? 1 : 0);
+    }    
+      
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {      	
+        if (preference == mKillAppLongpressBack) {
+            writeKillAppLongpressBackOptions();
+            return true;
+        }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-	    //Statusbar battery percentage
+	    ContentResolver resolver = getActivity().getApplicationContext().getContentResolver();
         if (preference == mNavigationBarHeight) {
             int statusNavigationBarHeight = Integer.valueOf((String) newValue);
             int index = mNavigationBarHeight.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+            Settings.System.putInt(resolver,
                     Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
             mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
             return true;
